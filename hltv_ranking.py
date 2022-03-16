7b0825dbb50695559e5f85484126f1c0e0804392
julien <git@julienc.io>
2022-03-16 23:04:34 +0100

    feat: name output file with ranking date

--- hltv_ranking.py
+++ hltv_ranking.py
@@ -1,33 +1,47 @@
 import json
 import sys
+from datetime import date
 from typing import Iterator
 
 import requests
 from bs4 import BeautifulSoup
+from dateutil.parser import parse
 
 
 RANKING_URL = "https://www.hltv.org/ranking/teams/"
 
 
-def iterate_teams() -> Iterator[str]:
+def get_ranking_html_content() -> BeautifulSoup:
     response = requests.get(RANKING_URL)
     assert response.status_code == 200
+    return BeautifulSoup(response.content, features="html.parser")
 
-    parsed_html = BeautifulSoup(response.content, features="html.parser")
-    for div in parsed_html.select(".ranking .ranked-team"):
+
+def iterate_teams(html_content: BeautifulSoup) -> Iterator[str]:
+    for div in html_content.select(".ranking .ranked-team"):
         yield div.select_one(".ranking-header .name").text
+
+
+def get_ranking_date(html_content: BeautifulSoup) -> date:
+    date_text = html_content.select_one(".regional-ranking-header").text.strip()
+    prefix = "CS:GO World ranking on "
+    date_text = date_text[len(prefix):]
+    return parse(date_text).date()
 
 
 def format_list(teams: Iterator[str]) -> str:
     return json.dumps(list(teams), indent=4)
 
 
-def main(output: str) -> None:
-    team_iterator = iterate_teams()
+def main(output: str) -> str:
+    html_content = get_ranking_html_content()
+    ranking_date = get_ranking_date(html_content)
+    team_iterator = iterate_teams(html_content)
     result = format_list(team_iterator)
 
     with open(output, "w") as f:
         f.write(result)
+    return ranking_date.isoformat()
 
 
 if __name__ == "__main__":
@@ -36,5 +50,5 @@ if __name__ == "__main__":
     except IndexError:
         print(f"Usage {sys.argv[0]} <output_path>")
         sys.exit(1)
-    main(output_path)
+    print(main(output_path))
 
