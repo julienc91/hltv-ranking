5086f76c469dbf1acb0878121499fd0c516a6259
julien <git@julienc.io>
2022-03-19 21:12:38 +0100

    chore: regroup logic in class

--- hltv_ranking.py
+++ hltv_ranking.py
@@ -41,12 +41,6 @@ class Player(TypedDict):
 T = TypeVar("T")
 
 
-def get_ranking_html_content() -> BeautifulSoup:
-    response = requests.get(RANKING_URL)
-    assert response.status_code == 200
-    return BeautifulSoup(response.text, features="html.parser")
-
-
 def _extract_attribute(
     team_div: Tag,
     selector: str,
@@ -58,92 +52,97 @@ def _extract_attribute(
     return parser(value)
 
 
-def get_teams(html_content: BeautifulSoup) -> list[Team]:
-    teams: list[Team] = []
-    for i, div in enumerate(html_content.select(".ranking .ranked-team"), start=1):
-        team: Team = {
-            "rank": i,
-            "name": _extract_attribute(div, ".ranking-header .name"),
-            "points": _extract_attribute(
-                div,
-                ".ranking-header .points",
-                parser=lambda val: int(re.sub(r"\D", "", val)),
-            ),
-            "change": _extract_attribute(
-                div,
-                ".ranking-header .change",
-                parser=lambda val: 0 if val == "-" else int(val),
-            ),
-            "logo_url": _extract_attribute(
-                div, ".team-logo img", getter=lambda tag: str(tag["src"])
-            ),
-            "players": get_players(div),
-            "url": _extract_attribute(
-                div,
-                ".lineup-con .more a.moreLink:not(.details)",
-                getter=lambda tag: str(tag["href"]),
-                parser=lambda val: f"https://www.hltv.org{val}",
-            ),
+class HLTVRanking:
+    BASE_URL = "https://www.hltv.org"
+    LATEST_RANKING_PATH = "/ranking/teams/"
+
+    def get_ranking_html_content(self) -> BeautifulSoup:
+        response = requests.get(self.BASE_URL + self.LATEST_RANKING_PATH)
+        assert response.status_code == 200
+        return BeautifulSoup(response.text, features="html.parser")
+
+    def get_teams(self, html_content: BeautifulSoup) -> list[Team]:
+        teams: list[Team] = []
+        for i, div in enumerate(html_content.select(".ranking .ranked-team"), start=1):
+            team: Team = {
+                "rank": i,
+                "name": _extract_attribute(div, ".ranking-header .name"),
+                "points": _extract_attribute(
+                    div,
+                    ".ranking-header .points",
+                    parser=lambda val: int(re.sub(r"\D", "", val)),
+                ),
+                "change": _extract_attribute(
+                    div,
+                    ".ranking-header .change",
+                    parser=lambda val: 0 if val == "-" else int(val),
+                ),
+                "logo_url": _extract_attribute(
+                    div, ".team-logo img", getter=lambda tag: str(tag["src"])
+                ),
+                "players": self.get_players(div),
+                "url": _extract_attribute(
+                    div,
+                    ".lineup-con .more a.moreLink:not(.details)",
+                    getter=lambda tag: str(tag["href"]),
+                    parser=lambda val: f"https://www.hltv.org{val}",
+                ),
+            }
+            teams.append(team)
+        return teams
+
+    def get_players(self, team_div: Tag) -> list[Player]:
+        players: list[Player] = []
+        for div in team_div.select(".lineup .player-holder"):
+            name: str = _extract_attribute(div, ".nick")
+            player: Player = {
+                "name": _extract_attribute(div, ".nick"),
+                "full_name": _extract_attribute(
+                    div,
+                    ".playerPicture",
+                    getter=lambda tag: str(tag["alt"]),
+                    parser=lambda val: val.replace(f" '{name}' ", " "),
+                ),
+                "country_code": _extract_attribute(
+                    div,
+                    ".flag",
+                    getter=lambda tag: str(tag["src"]),
+                    parser=lambda val: val.split("/")[-1].split(".")[0],
+                ),
+                "picture_url": _extract_attribute(
+                    div, ".playerPicture", getter=lambda tag: str(tag["src"])
+                ),
+                "url": _extract_attribute(
+                    div,
+                    "a.pointer",
+                    getter=lambda tag: str(tag["href"]),
+                    parser=lambda val: f"https://www.hltv.org{val}",
+                ),
+            }
+            players.append(player)
+        players.sort(key=lambda p: p["name"].lower())
+        return players
+
+    def get_ranking_date(self, html_content: BeautifulSoup) -> date:
+        date_text = html_content.select_one(".regional-ranking-header").text.strip()
+        prefix = "CS:GO World ranking on "
+        date_text = date_text[len(prefix) :]
+        return parse(date_text).date()
+
+    def format_export(self, ranking_date: date, teams: list[Team]) -> Ranking:
+        return {
+            "version": VERSION,
+            "date": ranking_date.isoformat(),
+            "source": "hltv.org",
+            "type": "world",
+            "teams": teams,
         }
-        teams.append(team)
-    return teams
 
-
-def get_players(team_div: Tag) -> list[Player]:
-    players: list[Player] = []
-    for div in team_div.select(".lineup .player-holder"):
-        name: str = _extract_attribute(div, ".nick")
-        player: Player = {
-            "name": _extract_attribute(div, ".nick"),
-            "full_name": _extract_attribute(
-                div,
-                ".playerPicture",
-                getter=lambda tag: str(tag["alt"]),
-                parser=lambda val: val.replace(f" '{name}' ", " "),
-            ),
-            "country_code": _extract_attribute(
-                div,
-                ".flag",
-                getter=lambda tag: str(tag["src"]),
-                parser=lambda val: val.split("/")[-1].split(".")[0],
-            ),
-            "picture_url": _extract_attribute(
-                div, ".playerPicture", getter=lambda tag: str(tag["src"])
-            ),
-            "url": _extract_attribute(
-                div,
-                "a.pointer",
-                getter=lambda tag: str(tag["href"]),
-                parser=lambda val: f"https://www.hltv.org{val}",
-            ),
-        }
-        players.append(player)
-    players.sort(key=lambda p: p["name"].lower())
-    return players
-
-
-def get_ranking_date(html_content: BeautifulSoup) -> date:
-    date_text = html_content.select_one(".regional-ranking-header").text.strip()
-    prefix = "CS:GO World ranking on "
-    date_text = date_text[len(prefix) :]
-    return parse(date_text).date()
-
-
-def format_export(ranking_date: date, teams: list[Team]) -> Ranking:
-    return {
-        "version": VERSION,
-        "date": ranking_date.isoformat(),
-        "source": "hltv.org",
-        "type": "world",
-        "teams": teams,
-    }
-
-
-def main() -> Ranking:
-    html_content = get_ranking_html_content()
-    ranking_date = get_ranking_date(html_content)
-    teams = get_teams(html_content)
-    return format_export(ranking_date, teams)
+    def export(self) -> Ranking:
+        html_content = self.get_ranking_html_content()
+        ranking_date = self.get_ranking_date(html_content)
+        teams = self.get_teams(html_content)
+        return self.format_export(ranking_date, teams)
 
 
 if __name__ == "__main__":
@@ -153,7 +152,8 @@ if __name__ == "__main__":
         print(f"Usage {sys.argv[0]} <output_path>")
         sys.exit(1)
 
-    data = main()
+    exporter = HLTVRanking()
+    data = exporter.export()
     with open(output_path, "w") as f:
         f.write(json.dumps(data, indent=4, ensure_ascii=False))
     print(data["date"])
